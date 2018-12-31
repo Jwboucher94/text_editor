8a795dcc0f9b76fffcf56734fe636569004e26ff
Jack Boucher <butticorn@gmail.com>
2018-12-31 11:17:33 +0100

    Add files via upload
    Version 1.3

--- README.md
+++ README.md
@@ -1,8 +1,41 @@
 # text_editor
 Python Text Editor
+Version: 1.3
 
-This is just a simple text editing program I'm working on in python. kind of completely useless given the terminals editing abilities as is, but it's a relatively simple command line to use. More options will be added with later releases.
+Welcome. This is a simple text editing program I'm working on in python. It's a relatively simple command line interface to use. As I think of them, I'll add more functionality. Functions are described below.
 
-Upon starting, you'll be asked to enter a folder name based on folders in current directory. You may create new folders if your folder selection does not exist, or enter a subfolder or previous folder. It will show you your current path and your parent path. 
+Main():
+  Used to run the program. activates on start.
 
-After, you'll be given the option to choose between 1 and 4, create, append, read, or change folder. after choosing an option, it will request a file name (except for change folder). if the file does not exist it will make a new file with that name. 
+listDirectory(path_local, parent_local):
+  Prints active path, parent path, and any folders in current path.
+  If no folders exist, tells you.
+
+pickDirectory(path_local, parent_local):
+  Pulls global path and parent directories, and asks for a directory. listDirectory is run to show what directories are available. If you leave input blank, it will ask if finished. if you are finished, it will set global settings and continue on. if not, you will start over. if you type "..", it will go to the parent directory. it will only show the full path if you go up from the file's directory. If folder name does not exist, it will ask if you would like to create.
+
+rawInput(text):
+  used as an alternative in some cases to clear console. only used a few times because of other lines needing to be displayed. might deprecate.
+
+job(response):
+  determines which program to use based on response number. If you do not type a number between 1 and 5, it will give an error. program will not reach job if you type a letter.
+
+fileNaming():
+  lists all available files in current directory. if you do not type a file, it will try again.
+
+finished():
+  simple program that just asks if you are done. if you type anything with a y, the program will exit. otherwise, will return.
+
+createFile(file_path, file_name):
+  creates a file.
+
+append(file_path, file_name):
+  asks for input, opens the file (creates, if necessary) and creates a new line of text in file.
+
+read(file_path, file_name):
+  Will try to read the file asked for. if it does not exist, it will create.
+
+question():
+  manages asking which option to select. If your response is blank, it will ask if you are finished. if you do not type a number, it will give an error.
+
+if __name__ == "__main__": starts main on start of program.
--- Text Modify.py
+++ Text Modify.py
@@ -1,159 +1,185 @@
 import os
-optargs = ["1. Create","2. Append","3. Read","4. Change Directory"];
+if os.name == "posix":
+	clear = "clear"
+elif os.name == "nt":
+	clear = "cls"
+optargs = [
+	"1. Create File",
+	"2. Append File",
+	"3. Read File",
+	"4. Change Directory",
+	"5. List directory"];
 argcount = "1-"+str(len(optargs));
 path = "./";
-last = (os.path.split(os.path.abspath("./"))[0]+"/");
+parent = (os.path.split(os.path.abspath("./"))[0]+"/");
 
 def main():
 	on = 1; #used for while loop
+	os.system(clear);
 	print(
-		"Welcome to my simple text creator and reader!\n"
-		"This is version 1.\n"
-		"I will add more features when my battery is not dying.\n"
+		"Project: Text Modifier\n"
+		"Version: 1.3\n"
+		"Welcome. This is a side project to practice python coding. "
 		"Enjoy!");
+	raw_input("Press enter to start...");
 	#following is used to list and determine directory folder and set path
 	global path;
-	global last;
-	print(pickDirectory(path, last));
+	global parent;
+	print(pickDirectory(path, parent));
 	#main while loop follows
 	while on > 0:
 		#ask which option to use
 		response = question();
-		if type(response) == int:
-			job(response);
-		else:
-			#if a number is not entered, starts loop over
-			print("Error: Please choose number " + argcount + ":");
-			on = 1;
+		job(response);
 
-def pickDirectory(path_local, last_local):
-	global path;
-	global last;
-	print("\nWhere shall we manage your files?");
+def listDirectory(path_local, parent_local):
 	print("Current Path: " + path_local);
-	print("Last Path: " + last_local);
-	for folder in next(os.walk(path_local))[1]:
-		print ("- " + folder);
+	print("Parent Path: " + parent_local);
+	walk = next(os.walk(path_local))[1];
+	if len(walk) == 0:
+		print("No folders in current path!");
+	else:
+		for folder in walk:
+			print ("- " + folder);
 	print("- ..");
+
+def pickDirectory(path_local, parent_local):
+	global path;
+	global parent;
+	os.system(clear);
+	print("Where shall we manage your files?");
+	listDirectory(path_local, parent_local);
 	folder = raw_input("Enter name of Directory (Use '..' to go back):\n>> ");
 	if len(folder) == 0:
-		answer = raw_input(
+		answer = rawInput(
 			"This is your path: " + path_local + ".\n"
 			"Any changes? (n/y)\n>>");
 		if answer == "y":
-			pickDirectory(path_local, last_local);
+			pickDirectory(path_local, parent_local);
 		else:
 			path = path_local;
-			last = last_local;
+			parent = parent_local;
 	elif folder == "..":
 		if path_local == "//":
-			pickDirectory(path, last);
+			pickDirectory(path, parent);
 		else:
-			if last == "./":
+			if parent == "./":
 				path = "./";
-				last = (os.path.split(os.path.abspath("./"))[0]+"/");
+				parent = (os.path.split(os.path.abspath("./"))[0]+"/");
 			else:
-				path = last;
-				last = (os.path.split(os.path.split(path)[0]))[0]+"/";
-			pickDirectory(path, last);
+				path = parent;
+				parent = (os.path.split(os.path.split(path)[0]))[0]+"/";
+			pickDirectory(path, parent);
 	else:
 		path_new = (path_local + folder + "/");
 		if not os.path.exists(path_new):
-			answer = raw_input("Folder does not exist. Create? (y/n)\n>>")
+			answer = rawInput("Folder does not exist. Create? (y/n)\n>>")
 			if answer == "n":
 				path = path_local;
-				last = last_local;
-				pickDirectory(path, last);
+				parent = parent_local;
+				pickDirectory(path, parent);
 			else:
 				os.makedirs(path_new);
-		answer = raw_input(
+		answer = rawInput(
 			"This is your path: " + path_new + ".\n"
 			"Any changes? (n/y)\n>>");
 		if answer == "y":
 			if path_new == (os.path.abspath("./")+"/"):
 				path = "./";
-				last = (os.path.split(os.path.abspath("./"))[0]+"/");
+				parent = (os.path.split(os.path.abspath("./"))[0]+"/");
 			else:
 				path = path_new;
-				last = path_local;
-			pickDirectory(path, last);
+				parent = path_local;
+			pickDirectory(path, parent);
 		else:
 			path = path_new;
-			last = path_local;
+			parent = path_local;
+
+def rawInput(text):
+	os.system(clear);
+	text = raw_input(text);
+	return text;
 
 def job(response):
+	global path;
+	global parent;
 	if response == 1:
-		filePath, fileName = fileNaming();
-		create(filePath, fileName);
+		file_path, file_name = fileNaming();
+		createFile(file_path, file_name);
 		finished();
+		return;
 	if response == 2:
-		filePath, fileName = fileNaming();
-		append(filePath, fileName);
+		file_path, file_name = fileNaming();
+		append(file_path, file_name);
 		finished();
+		return;
 	if response == 3:
-		filePath, fileName = fileNaming();
-		read(filePath, fileName);
+		file_path, file_name = fileNaming();
+		read(file_path, file_name);
 		finished();
+		return;
 	if response == 4:
-		global path
-		global last
-		pickDirectory(path, last);
+		pickDirectory(path, parent);
+		finished();
+		return;
+	if response == 5:
+		listDirectory(path, parent);
+		finished();
+		return;
 	else:
-		print("Error: Please choose number " + argcount + ":");
+		rawInput(
+			"Error: Options are " + argcount + ". "
+			"\nPress enter to continue...");
+		return;
 
 def fileNaming():
+	os.system(clear);
 	print ("Available files:");
 	for files in next(os.walk(path))[2]:
 		print("- "+str(os.path.splitext(files)[0]));
 	print("");
-	fileName = str(raw_input("Please enter file name: \n>> "))
-	filePath = path + fileName
-	while len(filePath) == 0:
+	file_name = str(raw_input("Please enter file name: \n>> "))
+	file_path = path + file_name
+	if len(file_name) == 0:
 		print("Please enter a file name");
 		fileNaming();
-	return filePath, fileName
+	return file_path, file_name
 
 def finished():
- 	response= raw_input("Are you finished? (y/n)\n>> ");
-	if response == "n":
-		return;
-	if response == "y":
+ 	response = raw_input("Are you finished? (y/n)\n>> ");
+	if "y" in response:
 		exit();
 	else:
-		print("Answer with y or n.");
-		finished();
+		return;
 
-def crFolder():
-	path = "/files"
-	os.mkdir(path)
-	on = finished();
-	return on;
-
-def create(filePath, fileName):
-	file = open(filePath + ".txt", "w+");
+def createFile(file_path, file_name):
+	file = open(file_path + ".txt", "w+");
 	file.close();
-	print('Text file created with the name "' + fileName + '"');
+	print('Text file created with the name "' + file_name + '"');
 
-def append(filePath, fileName):
-	text = raw_input("Enter text:\n>> ");
-	file = open(filePath + ".txt", "a");
+def append(file_path, file_name):
+	text = rawInput("Enter text:\n>> ");
+	file = open(file_path + ".txt", "a");
 	file.write("\r\n" + text);
 	file.close();
+	os.system(clear);
 	print("Text file appended with " + '"' + text + '"');
 
-def read(filePath, fileName):
+def read(file_path, file_name):
 	try:
-		file = open(filePath + ".txt", "r");
-		print("File Name: " + fileName + "\n--------" + file.read());
+		file = open(file_path + ".txt", "r");
+		os.system(clear);
+		print("File Name: " + file_name + "\n--------" + file.read());
 		file.close();
 	except IOError:
-		ask = raw_input("File does not exist... Create? (y/n)\n>> ")
+		ask = rawInput("File does not exist... Create? (y/n)\n>> ")
 		if ask == "n":
 			True;
 		else:
-			create(filePath, fileName);
+			createFile(file_path, file_name);
 
 def question():
+	os.system(clear);
 	print("Choose One " + argcount + ":");
 	for arg in optargs:
 		print (arg);
@@ -162,8 +188,12 @@ def question():
 		response = int(response);
 	except ValueError:
 		if response == "":
-			finished()
+			finished();
+			question();
 		else:
+			rawInput(
+				"Error: Options are " + argcount + ". "
+				"\nPress enter to continue...");
 			question();
 	return response;
 
